bfa4d51ed9d4d46b1a40e1aa26a7dfcbfadbdfd0
GuerSa <100643304+GuerSa@users.noreply.github.com>
2023-04-16 22:56:00 -0700

    Update anova.mars.R

--- mars/R/anova.mars.R
+++ mars/R/anova.mars.R
@@ -2,33 +2,38 @@
 
 anova.mars <- function(object,...){
   cat("Analysis of Variance Table\n\nResponse: y\n")
-  cat("\tDf\tSum Sq\tMean Sq\tF value\tPr(>F)\n")
-  for(i in 1:length(object$Bfuncs)){
-    cat(names(object$coefficients)[i],"\t")
-    
-    ######## Didn't finish yet:(
-    cat("\t",)                 # sum sq
-    cat(,"\t")                 # mean sq
-    fvalue <-                  # F value
-    cat(fvalue,"\t")           # Pr(>F)
-    f <- 
-    ########
-    if(f < 2.2e-16){
-      cat("<2.2e-16   ")
-    }else{
-      cat(f,"   ")
-    }
-    if(f < 0.001){
-      cat("***")
-    } else if(f < 0.01){
-      cat("**")
-    } else if(f < 0.05){
-      cat("*")
-    } else if(f < 0.1){
-      cat(".")
-    }
-    cat("\n")
-  }
+  df <- c(rep(1, object$rank), df.residual(object))
+  ssr <- sum(object$residuals^2)
+  ss <- round(c(unname(object$effects[1:object$rank]^2), ssr))
+  ms <- round(ss/df)
+  fval <- round(ms/(ssr/df.residual(object)), digits = 2)
+  fval[length(fval)] <- NA
+  pr <- pf(fval, df, df.residual(object), lower.tail = FALSE)
+  pr[length(pr)] <- NA
+  
+  cat(format("", width = nchar("residuals")), 
+      format("Df", width = pmax(nchar("Df"), nchar(max(df))), justify = "right"),
+      format("Sum Sq", width = pmax(nchar("Sum Sq"), nchar(max(ss))), justify = "right"),
+      format("Mean Sq", width = pmax(nchar("Mean Sq"), nchar(max(ms))), justify = "right"),
+      format("F Value", width = pmax(nchar("F Value"), nchar(max(fval, na.rm = TRUE))), justify = "right"),
+      format("Pr(>F)", width = nchar("< 2.2e-16"), justify = "right"),
+      "\n")
+
+  for(i in object$assign) {
+  cat(format(names(object$coefficients)[i], width = nchar("Residuals")), 
+      format(df[i], width = pmax(nchar("Df"), nchar(max(df))), justify = "right"),
+      format(ss[i], width = pmax(nchar("Sum Sq"), nchar(max(ss))), justify = "right"),
+      format(ms[i], width = pmax(nchar("Mean Sq"), nchar(max(ms))), justify = "right"),
+      format(fval[i], width = pmax(nchar("F Value"), nchar(max(fval, na.rm = TRUE))), justify = "right"),
+      format(pr[i], width = pmax(nchar("Pr(>F)"), nchar("< 2.2e-16")), justify = "right"),
+      "\n")
+}
+  cat(format("Residuals", width = nchar("Residuals")), 
+      format(df[length(df)], width = pmax(nchar("Df"), nchar(max(df))), justify = "right"),
+      format(ss[length(df)], width = pmax(nchar("Sum Sq"), nchar(max(ss))), justify = "right"),
+      format(ms[length(df)], width = pmax(nchar("Mean Sq"), nchar(max(ms))), justify = "right"))
+  cat("\n")
+  
   # cat("Residuals\t", object$df.residual,"\t","\t","\n")
   cat("---\nSignif. codes: 0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1\n\n")
   
